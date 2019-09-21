380fe7cbe6baf90921a64e01143b8b6003c6ad2a
aestheticJL <m258789@126.com>
2019-09-21 16:35:21 +0800

    first commit

---
+++ src/main/java/com/mmt/ltxm/LtxmApplication.java
@@ -0,0 +1,13 @@
+package com.mmt.ltxm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LtxmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LtxmApplication.class, args);
+    }
+
+}
